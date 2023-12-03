7ea4df6b42e4523930475c9120625abb1cede234
ghanashyam03 <ghanashyamvn@gmail.com>
2023-12-04 02:59:50 +0530

    group not done

---
+++ server/app/group_selection.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+from flask_cors import CORS
+from . import app, mysql
+
+CORS(app)
+
+@app.route('/api/donors/bloodgroup', methods=['POST'])
+def get_donors_by_blood_group():
+    try:
+        data = request.get_json()
+        blood_group = data.get('blood_group')
+        print('Received blood group:', blood_group)
+
+        cur = mysql.connection.cursor(dictionary=True)
+        cur.execute("SELECT * FROM donor WHERE blood_group = %s", (blood_group,))
+        donors = cur.fetchall()
+        cur.close()
+
+        print('Returning donors:', donors)
+
+        return jsonify(donors), 200
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        print(f"Error fetching donors: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
--- src/components/Hospital/BloodGroupSelection.js
+++ src/components/Hospital/BloodGroupSelection.js
@@ -1,26 +1,53 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const BloodGroupSelection = () => {
   const [selectedBloodGroup, setSelectedBloodGroup] = useState('');
+  const [donors, setDonors] = useState([]);
 
-  const handleSelection = (e) => {
-    // Add logic to handle blood group selection
-    setSelectedBloodGroup(e.target.value);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Submitting form with blood group:', selectedBloodGroup);
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/donors/bloodgroup', {
+        blood_group: selectedBloodGroup,
+      });
+
+      console.log('Response:', response.data);
+
+      setDonors(response.data);
+    } catch (error) {
+      console.error('Error fetching donors:', error);
+    }
   };
 
   return (
     <div>
       <h2>Blood Group Selection</h2>
-      <label>Select Blood Group:</label>
-      <select value={selectedBloodGroup} onChange={handleSelection}>
-        <option value="A+">A+</option>
-        <option value="A-">A-</option>
-        <option value="B+">B+</option>
-        <option value="B-">B-</option>
-        {/* Add more blood group options as needed */}
-      </select>
+      <form onSubmit={handleSubmit}>
+        <label>Select Blood Group:</label>
+        <select value={selectedBloodGroup} onChange={(e) => setSelectedBloodGroup(e.target.value)}>
+          <option value="A+">A+</option>
+          <option value="A-">A-</option>
+          <option value="B+">B+</option>
+          <option value="B-">B-</option>
+          {/* Add more blood group options as needed */}
+        </select>
+        <button type="submit">Get Donors</button>
+      </form>
 
-      {/* Display the list of donors or handle further actions based on the selection */}
+      {donors.length > 0 && (
+        <div>
+          <h3>Donors with {selectedBloodGroup} Blood Group:</h3>
+          <ul>
+            {donors.map((donor) => (
+              <li key={donor.id}>{donor.name}</li>
+              // Adjust this based on your donor structure
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
