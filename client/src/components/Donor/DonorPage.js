ae4dccce693bbb8bd253f786d8eb897743012253
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-12-04 16:42:19 +0530

    frontend done

--- client/src/components/Donor/DonorPage.js
+++ client/src/components/Donor/DonorPage.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
-const DonorPage = ({ donorData }) => {
-  // Assuming donorData is an object containing donor information
-  const { name, phone, bloodGroup } = donorData;
-
+export default function Example() {
   return (
-    <div>
-      <h2>Donor Information</h2>
-      <p>Name: {name}</p>
-      <p>Phone: {phone}</p>
-      <p>Blood Group: {bloodGroup}</p>
-      {/* Add more information as needed */}
-    </div>
-  );
-};
+    <div className="relative isolate overflow-hidden bg-white">
+      <svg
+        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+            width={200}
+            height={200}
+            x="50%"
+            y={-1}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" strokeWidth={0} fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" />
+      </svg>
+      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-24">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
 
-export default DonorPage;
+          <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Thankyou Dheeraj
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+          Thank you for joining our blood donation community – your registration brings hope and saves lives
+          </p>
+        </div>
+        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <img
+                src="https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="App screenshot"
+                className="w-[50rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/src/components/Hospital/BloodGroupSelection.js
+++ client/src/components/Hospital/BloodGroupSelection.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid'
 import axios from 'axios';
 
 const BloodGroupSelection = () => {
@@ -22,9 +23,49 @@ const BloodGroupSelection = () => {
     }
   };
 
+  const dummyDonors = [
+    {
+      name: 'Jane Cooper',
+      title: 'Regional Paradigm Technician',
+      role: 'Admin',
+      email: 'janecooper@example.com',
+      telephone: '+1-202-555-0170',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+      name: 'Jane Cooper',
+      title: 'Regional Paradigm Technician',
+      role: 'Admin',
+      email: 'janecooper@example.com',
+      telephone: '+1-202-555-0170',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+      name: 'Jane Cooper',
+      title: 'Regional Paradigm Technician',
+      role: 'Admin',
+      email: 'janecooper@example.com',
+      telephone: '+1-202-555-0170',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+      name: 'Jane Cooper',
+      title: 'Regional Paradigm Technician',
+      role: 'Admin',
+      email: 'janecooper@example.com',
+      telephone: '+1-202-555-0170',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    // More people...
+  ];
+
   return (
     <div className="bg-white py-16 sm:py-24 lg:py-32">
-    <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-2 lg:gap-8 lg:px-8">
+    <div className="mx-5 grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-2 lg:gap-8 lg:px-8">
       <div className="max-w-xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7">
         <h2 className="inline sm:block lg:inline xl:block">Select the blood group</h2>{' '}
         {/* <p className="inline sm:block lg:inline xl:block">Sign up for our newsletter.</p> */}
@@ -54,11 +95,45 @@ const BloodGroupSelection = () => {
     </div>
     {donors.length > 0 && (
         <div>
-          <h3>Donors with {selectedBloodGroup} Blood Group:</h3>
-          <ul>
-            {donors.map((donor) => (
-              <li key={donor.id}>{donor.name}</li>
-              // Adjust this based on your donor structure
+          <h3 className='m-10 text-2xl font-semibold'>Donors with {selectedBloodGroup} Blood Group:</h3>
+          <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 p-10">
+            {donors.map((person) => (
+              <li key={person.email} className="col-span-1 divide-y border-2 border-gray-100 shadow-lg divide-gray-200 rounded-lg bg-white ">
+                <div className="flex w-full items-center justify-between space-x-6 p-6">
+                  <div className="flex-1 truncate">
+                    <div className="flex items-center space-x-3">
+                      <h3 className="truncate text-sm font-medium text-gray-900">{person.name}</h3>
+                      {/* <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                        {person.role}
+                      </span> */}
+                    </div>
+                    {/* <p className="mt-1 truncate text-sm text-gray-500">{person.title}</p> */}
+                  </div>
+                  <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={person.imageUrl} alt="" />
+                </div>
+                <div>
+                  <div className="-mt-px flex divide-x divide-gray-200">
+                    <div className="flex w-0 flex-1">
+                      <a
+                        href={`mailto:${person.email}`}
+                        className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                      >
+                        <EnvelopeIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        Email
+                      </a>
+                    </div>
+                    <div className="-ml-px flex w-0 flex-1">
+                      <a
+                        href={`tel:${person.telephone}`}
+                        className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                      >
+                        <PhoneIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        Call
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
             ))}
           </ul>
         </div>
